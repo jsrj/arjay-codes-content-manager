169690ce5a1b599a3baba353ce34f0a1faac819e
Arjay Jones <robert.jones@arjaycodes.com>
2019-06-03 16:37:33 -0700

    added test public and private routes; added JWT authorization check

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 // Set up express
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const db = require('./api/db/connection')
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
 // Body parser
 const bodyParser = require('body-parser')
@@ -27,11 +30,33 @@ app.use(function (req, res, next) {
     next();
 });
 
+// Authorization check
+const authCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: process.env.JWKS_URI
+    }),
+    // This is the identifier we set when we created the API
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: process.env.AUTH0_ISSUER,
+    algorithms: ['RS256']
+});
+
 // Import routes
-const about = require('./api/routes/about');
+// const about = require('./api/routes/about');
 
 // Use routes
-app.use('/about', about);
+// app.use('/about', about);
+// Public test route
+app.get('/api/public', (req, res) => {
+    res.json({message: 'hello from public route'});
+});
+// Private test route
+app.get('/api/private', authCheck, (req,res) => {
+    res.json({message: 'hello from private route'});
+});
 
 // Listen for the server at a port.
 app.listen(process.env.PORT || 8000, (err) => {
